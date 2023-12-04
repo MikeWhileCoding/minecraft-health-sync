92d62ff8080bf13de4198655afdd4c50cf59df6b
Mike de Groot <mike251105@gmail.com>
2023-12-04 22:16:12 +0100

    :tada: Start of project

---
+++ src/main/java/me/mikewhilecoding/minecraft_health_sync/MinecraftHealthSync.java
@@ -0,0 +1,17 @@
+package me.mikewhilecoding.minecraft_health_sync;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MinecraftHealthSync extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
